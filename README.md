0a7d447c25f7edcc4c8dde0c3fd1e12f1d949e1b
alexdcdc <69087503+alexdcdc@users.noreply.github.com>
2023-03-07 14:44:28 -0500

    Added test scripts and requirements.txt

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # AppsForGood
 AFG Project for Spring 2023
-
-A few dependencies needed to get started working with this project:
-
-Python: https://www.python.org/downloads/
-
-spaCy: "pip install spacy" in shell
-
-NLTK: "pip install nltk" in shell
+by Alex Chen, Thomas Park, and Shivani Parmar
---
+++ TestScripts/spaCyTest.py
@@ -0,0 +1,31 @@
+import spacy
+
+from collections import Counter
+from string import punctuation
+
+nlp = spacy.load("en_core_web_sm")
+
+def get_hotwords(text):
+    result = []
+    pos_tag = ['PROPN', 'ADJ', 'NOUN'] 
+    doc = nlp(text.lower()) 
+    for token in doc:
+        if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
+            continue
+        if(token.pos_ in pos_tag):
+            result.append(token.text)
+    return result
+
+
+fh = open("TestScripts\\Example.txt", "r")
+
+new_text = fh.read()
+
+output = set(get_hotwords(new_text))
+
+most_common_list = Counter(output).most_common(10)
+
+for item in most_common_list:
+  print(item[0])
+
+fh.close()
